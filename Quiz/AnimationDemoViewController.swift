86cedaeaadfbfb48b9308170efd5c45c6e582fcb
Nikolay Shubenkov <n.shubenkov@be-interactive.ru>
2016-04-16 11:51:33 +0300

    my first animation. Uhooo

---
+++ Quiz/AnimationDemoViewController.swift
@@ -0,0 +1,51 @@
+//
+//  AnimationDemoViewController.swift
+//  Quiz
+//
+//  Created by Nikolay Shubenkov on 16/04/16.
+//  Copyright © 2016 Nikolay Shubenkov. All rights reserved.
+//
+
+import UIKit
+
+class AnimationDemoViewController: UIViewController {
+
+    @IBOutlet weak var firstYellowView: UIView!
+    
+    @IBOutlet weak var vandamView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//      Простая анимация доступна для следующих параметров у UIView и его наследников:
+//        1. alpha - прозрачность
+//        2. Положение вьюхи и ее размер
+//        3. цвет фона
+//        4. tranasform - специальная структура, которая определяет положение, вращение у вьюхи и масштаб. Осторжно используйте ее с AutoLayout потому, что они не всегда друг с другом ладят
+        
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        prepareForAnimation()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        animateAppearing()
+    }
+    
+//  MARK: - Animation
+    
+    func prepareForAnimation(){
+        vandamView.alpha = 0
+        firstYellowView.backgroundColor = UIColor.greenColor()
+    }
+    
+    func animateAppearing(){
+        UIView.animateWithDuration(0.3) { 
+            self.vandamView.alpha = 1
+            self.firstYellowView.backgroundColor = UIColor.yellowColor()
+        }
+    }
+
+}
